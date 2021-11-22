b34e56097071562b1b13423d18dfa92c6a00b62d
Thomas Mello <thomasmelison@hotmail.com>
2021-11-21 23:06:50 -0300

    Commit com o javadoc

--- CaixaDeEntrada.java
+++ CaixaDeEntrada.java
@@ -1,27 +1,52 @@
 import java.util.ArrayList;
+/**
+ * Classe CaixaDeEntrada simula uma caixa de entrada. Tem uma Array List
+ * que guarda as mensagens e tem a mens_index_erro que é uma mensagem default 
+ * para um erro ao acessar um índice inexistente na Array List Mensagens.
+ */
 public class CaixaDeEntrada {
 
     private ArrayList<Mensagem> Mensagens = new ArrayList<Mensagem>();
-    private Mensagem mens_default;
+    private Mensagem mens_index_erro;
     
+    /**
+     * Construtor que cria a caixa de entrada e também cria a 
+     * mensagem de erro de índice.
+     */
     public CaixaDeEntrada(){
-        mens_default = new Mensagem();
+        mens_index_erro = new Mensagem("Mensagem não encontrada (índice não encontrado) ...");
     }
 
+    /**
+     * Metodo que adiciona uma mensagem a Array List Mensagens.
+     * @param m é a mensagem a ser adicionada a Caixa de Entrada.
+     */
     public void adicionaMensagem(Mensagem m){
         Mensagens.add(m);
     }
+
+    /**
+     * Metodo que acessa uma mensagem da caixa de entrada com base em seu índice
+     * e retorna o objeto mensagem.
+     * @param i é o índice da mensagem a ser acessada.
+     * @return retorna o objeto mensagem.
+     */
     public Mensagem acessaMensagem(int i){
         if( i < 0 || i > Mensagens.size() - 1){
-            return mens_default;
+            return mens_index_erro;
         }
         else{
             return Mensagens.get(i);
         }
     }
+
+    /**
+     * Metodo que remove uma mensagem da caixa de entrada (remove o objeto da Array List Mensagens).
+     * @param i é o índice da mensagem a ser removida.
+     */
     public void removeMensagem(int i){
         if( i < 0 || i > Mensagens.size() - 1){
-            System.out.println(mens_default.toString());
+            System.out.println(mens_index_erro.toString());
         }
         else{
             Mensagens.remove(i);
--- Mensagem.java
+++ Mensagem.java
@@ -1,3 +1,7 @@
+/**
+ * A classe mensagem guarda individualmente as mensagens
+ * com o nome do remetente e do destinatário.
+ */
 public class Mensagem {
     
     private String remetente;
@@ -7,18 +11,37 @@ public class Mensagem {
     /**
      * Mensagem default para Mensagem nao encontrada.
      */
-    public Mensagem(){
-        mensagem = "Mensagem não encontrada...";
+    public Mensagem(String erro){
+        mensagem = erro;
     }
-
+    
+    /**
+     * Construtor que cria a mensagem
+     * @param remetente string que guarda o nome de quem mandou a mensagem
+     * @param destinatario string que guarda o nome de quem recebeu a mensagem
+     * @param mensagem string que guarda a mensagem em si
+     */
     public Mensagem(String remetente, String destinatario, String mensagem){
         this.remetente = remetente;
         this.destinatario = destinatario;
         this.mensagem = mensagem;
     }
+    
+    /**
+     * Função que anexa uma linha de texto a mensagem
+     * @param linha_de_texto é a string que será concatenada na mensagem
+     */
     public void anexa(String linha_de_texto){
         mensagem = mensagem + "\n" + linha_de_texto;
     }
+
+    /**
+     * Função que retorna uma string com o remetente, o destinatário e a mensagem.
+     * Pode retornar erro caso o remetente e o destinatário sejam nulos, que será o 
+     * caso para a um objeto criado pelo construtor Mensagem(). Esta mensagem é
+     * default para quando algum erro de indice inválido.
+     * @return 
+     */
     public String toString(){
         if(remetente == null|| destinatario == null ){
             return mensagem;
